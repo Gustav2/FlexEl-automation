00511bce11056d87a5d871bba86458a31a9a46c4
Gustav2 <gustavnybro@protonmail.com>
2022-06-06 01:09:23 +0200

    Better comments + typeends

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
-url = "https://norlys.dk/kundeservice/el/flexel-prisudvikling"
+URL = "https://norlys.dk/kundeservice/el/flexel-prisudvikling"
 
 """
 Adds option to enable headless mode.
@@ -17,7 +17,7 @@ options.add_argument("--headless")
 drive = webdriver.Firefox(options=options)
 
 # Fetches page
-drive.get(url)
+drive.get(URL)
 
 # Sleep to make sure the page is loaded
 t.sleep(2)
@@ -26,10 +26,13 @@ t.sleep(2)
 content = BeautifulSoup(drive.page_source, "html.parser")
 
 
-# Parses HTML, finds cheapest timeinterval and returns timeinterval in ISO format
-def find_cheapest_time():
+def find_cheapest_time() -> list:
+    """
+    Parses HTML, finds cheapest timeinterval and returns timeinterval in ISO format
+    Returns ['HH:MM', 'HH:MM']
+    Else returns 'None' if no timeinterval is found
+    """
     root = content.find("div", {"id": "root"})
-    # cheapest = root.find(lambda tag:tag.name=="p" and "time" in tag.text)
     cheapest = root.find("div", {"data-testid": "flex-el_cheapest-price-indicator"})
 
     for child in cheapest.children:
@@ -41,9 +44,11 @@ def find_cheapest_time():
                     timeinterval.append(i)
 
             return [i[:2] + ":" + i[2:] for i in timeinterval]
+        else:
+            return ["No timeinterval found"]
 
 
-def check_time_in_interval(ti):
+def check_time_in_interval(ti: list) -> bool:
     return time.fromisoformat(ti[0]) <= datetime.now().time() <= time.fromisoformat(ti[1])
 
 
